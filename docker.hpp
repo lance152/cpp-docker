ebdd5d4c58d96fb4058f9287ee4a95c6bcb1e32d
Runzhou Fan <lance9040@gmail.com>
2020-05-24 00:37:19 -0400

    Add network support

--- docker.hpp
+++ docker.hpp
@@ -8,6 +8,10 @@
 
 #include <string> //C++标准库
 
+#include <net/if.h> //if_nametoindex
+#include <arpa/inet.h> //inet_pton
+#include "nework.h"
+
 #define STACK_SIZE (512*512) //定义子进程空间大小
 
 namespace docker{
@@ -20,6 +24,9 @@ namespace docker{
   struct container_config{
     std::string host_name; //主机名
     std::string root_dir; //根目录
+    std::string ip; //容器ip
+    std::string bridge_name; //网桥名
+    std::string bridge_ip; //网桥ip
   };
 
   class container{
@@ -32,6 +39,9 @@ namespace docker{
 
     //容器配置
     container_config config;
+
+    //保存容器网络设备，用于删除
+    char *veth1,*veth2;
 
     //docker run -it ubuntu:14.04 /bin/bash 可以让一个容器保持运行状态
     //我们在这里定义一个start_bash函数来执行这个命令
@@ -66,6 +76,36 @@ namespace docker{
       mount("none","/proc","proc",0,NULL);
       mount("none","/sys","sysfs",0,NULL);
     }
+
+    //设置网络参数
+    void set_network(){
+      int ifindex = if_nametoindex("eth0");
+      struct in_addr ipv4;
+      struct in_addr bcast;
+      struct in_addr gateway;
+
+      //ip地址转换函数，将ip地址在点分十进制和二进制之间进行转换
+      inet_pton(AF_INET,this->config.ip.c_str(),&ipv4);
+      inet_pton(AF_INET,"255.255.255.0".&bcast);
+      inet_pton(AF_INET,this->config.bridge_ip.c_str(),&gateway);
+
+      //配置eth0 ip地址
+      lxc_ipv4_addr_add(ifindex,&ipv4,&bcast,16);
+
+      //激活lo
+      lxc_netdev_up("lo");
+
+      //激活eth0
+      lxc_netdev_up("eth0");
+
+      //设置网关
+      lxc_ipv4_gateway_add(ifindex,&gateway);
+
+      //设置eth0的MAC地址
+      char mac[18];
+      new_hwaddr(mac);
+      setup_hw_addr(mac,"eth0");
+    }
   public:
     //构造函数
     container(container_config &config){
@@ -73,6 +113,23 @@ namespace docker{
     }
 
     void start(){
+      char veth1buf[IFNAMSIZ] = "shiyanlou0X";
+      char veth2buf[IFNAMSIZ] = "shiyanlou0X";
+
+      //创建一对网络设备，一个用于加载到宿主机，另一个用于转移到子进程容器中
+      veth1 = lxc_mkifname(veth1buf);//这里调用了第三方库，network.h中的函数
+      veth2 = lxc_mkifname(veth2buf);
+      lxc_veth_create(veth1,veth2);
+
+      //设置veth1的MAC地址
+      setup_private_host_hw_addr(veth1);
+
+      //将veth1添加到网桥中
+      lxc_bridge_attach(config.bridge_name.c_str(),veth1);
+
+      //激活veth1
+      lxc_netdev_up(veth1);
+
       auto setup = [](void *args)->int{
         auto _this = reinterpret_cast<container*>(args);
 
@@ -80,16 +137,25 @@ namespace docker{
         _this->set_hostname();
         _this->set_rootdir();
         _this->set_procsys();
+        _this->set_network();
         _this->start_bash();
 
         return proc_wait;
       };
 
-      process_pid child_pid = clone(setup,child_stack+STACK_SIZE,CLONE_PID | CLONE_NEWNS | CLONE_NEWUTS | SIGCHLD,this);
+      process_pid child_pid = clone(setup,child_stack+STACK_SIZE,CLONE_NEWNET | CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWUTS | SIGCHLD,this);
       //首先child_stack+STACK_SIZE会移动到栈底，SIGCHLD会在子进程退出时发出信号给父进程
       //这里传入了this指针是为了让etup函数获得contaier对象
       //这里setup函数未lambda函数，C++中捕获列表为空的lambda函数可以作为函数指针传递
+
+      //将veth2移到容器中
+      lxc_netdev_move_by_name(veth2,child_pid,"eth0");
       waitpid(child_pid,NULL,0); //等待子进程退出
+    }
+
+    ~container(){
+      lxc_netdev_delete_by_name(veth1);
+      lxc_netdev_delete_by_name(veth2);
     }
   };
 }
--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@ int main(int argc, char** argv){
   config.host_name = "lfrz";
   config.root_dir = "./dock";
 
+  //配置网络参数
+  config.ip = "192.168.0.100"; // 容器ip
+  config.bridge_name = "docker0"; //宿主机网桥名
+  config.bridge_ip = "192.168.0.1"; //宿主机网桥ip
+
   docker::container container(config);//构造容器
   container.start();
   std::cout<<"stop container..."<<std::endl;
